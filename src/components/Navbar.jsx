101397fb04126c1fd71893c7dc49114045c291e6
panickus <felipedg@outlook.es>
2024-03-19 00:24:39 +0100

    Development the Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import { appleImg } from '../utils';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple logo" width={14} height={18}/>
+
+        <div>
+          {['iPhones', 'Macs', 'iPads', 'Accessories'].map((nav, i) => (
+            <div>
+              {nav}
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
   )
 }
 
