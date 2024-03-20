9b33bc189f0e9d8fae75e52dc012c16f72de5ac7
panickus <felipedg@outlook.es>
2024-03-20 17:47:29 +0100

    Complete Features component.

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,12 +1,12 @@
-import gsap from "gsap"
+import gsap from "gsap";
 
-import { ScrollTrigger } from "gsap/all"
+import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
 export const animateWithGsap = (target, animationProps, scrollProps) => {
   gsap.to(target, {
     ...animationProps,
-    scrollTrigger: {
+      scrollTrigger: {
       trigger: target,
       toggleActions: 'restart reverse restart reverse',
       start: 'top 85%',
